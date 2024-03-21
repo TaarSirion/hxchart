6f579053297f74419418349447da1e83216a4516
unknown <taarsirion@gmail.com>
2024-03-21 20:29:05 +0100

    tests: add more tests for axis calc

--- Source/tests/TestAxis.hx
+++ Source/tests/TestAxis.hx
@@ -8,20 +8,40 @@ class TestAxis extends Test {
 	function testSetTickInfo() {
 		var tick_info = AxisTools.calcTickInfo(0, 1);
 
-		Assert.equals(3, tick_info.num);
-		Assert.equals(0, tick_info.min);
-		Assert.equals(1, tick_info.pos_ratio);
-		Assert.equals(0, tick_info.prec);
-		Assert.equals(1, tick_info.step);
-		Assert.equals(0, tick_info.zero);
+		Assert.same({
+			num: 3,
+			min: 0,
+			pos_ratio: 1,
+			prec: 0,
+			step: 1,
+			zero: 0
+		}, tick_info);
 
 		var tick_info = AxisTools.calcTickInfo(-1, 1);
 
-		Assert.equals(5, tick_info.num);
-		Assert.equals(-2, tick_info.min);
-		Assert.equals(0.5, tick_info.pos_ratio);
-		Assert.equals(0, tick_info.prec);
-		Assert.equals(1, tick_info.step);
-		Assert.equals(2, tick_info.zero);
+		Assert.same({
+			num: 5,
+			min: -2,
+			pos_ratio: 0.5,
+			prec: 0,
+			step: 1,
+			zero: 2
+		}, tick_info);
+	}
+
+	function testCalcTickPos() {
+		var pos = AxisTools.calcTickPos(2, 10, 0, false);
+		Assert.equals(0, pos[0]);
+		Assert.equals(10, pos[1]);
+	}
+
+	function testCalcSubTickInfo() {
+		var subtickinfo = AxisTools.calcSubTickInfo(10, 2, 1);
+
+		Assert.same({
+			dists: 5,
+			step: 0.5,
+			prec: 1
+		}, subtickinfo);
 	}
 }
